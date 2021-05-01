1f1c61d7252b3ac5b67f925865e7cf135c4d6e1b
CaubiLoureiro <79065406+CaubiLoureiro@users.noreply.github.com>
2021-05-01 14:05:08 -0300

    Add files via upload

---
+++ ObjetoProcesso/PessoaFisica.java
@@ -0,0 +1,20 @@
+package ObjetoProcesso;
+
+public class PessoaFisica extends Processo{
+
+    private String cpf;
+
+    public PessoaFisica(String reu, String autor, String numeroProcesso, String advogadoAutor, String advogadoReu, String cpf){
+        super(reu, autor, numeroProcesso, advogadoAutor, advogadoReu);
+        this.cpf = cpf;
+
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ ObjetoProcesso/PessoaJuridica.java
@@ -0,0 +1,20 @@
+package ObjetoProcesso;
+
+public class PessoaJuridica extends Processo{
+
+    private String cnpj;
+
+    public PessoaJuridica(String reu, String autor, String numeroProcesso, String advogadoAutor, String advogadoReu, String cnpj){
+        super(reu, autor, numeroProcesso, advogadoAutor, advogadoReu);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
